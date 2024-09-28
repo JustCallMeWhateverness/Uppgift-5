a1f53e6fabdb0a9030750c76960004529fd380fe
JustCallMeWhateverness <nicolefritz00@gmail.com>
2024-09-28 17:27:30 +0200

    Initial commit: Made a simple calculator

--- Program.cs
+++ Program.cs
@@ -6,10 +6,39 @@ using System.Threading.Tasks;
 
 namespace Uppgift_5
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Simple Calculator");
+            Console.WriteLine("Enter (+, -, *,/):");
+            char type = Console.ReadKey().KeyChar;
+            Console.WriteLine("\nEnter the first number: ");
+            double number1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the second number: ");
+            double number2 = Convert.ToDouble(Console.ReadLine());
+
+            double result = 0;
+            switch (type)
+            {
+                case '+':
+                    result = number1 + number2;
+                    break;
+                case '-':
+                    result = number1 - number2;
+                    break;
+                case '*':
+                    result = number1 * number2;
+                    break;
+                case '/':
+                    result = number1 / number2;
+                    break;
+                default:
+                    Console.WriteLine("Invalid type.");
+                    return;
+            }
+            Console.WriteLine($"Result: {result}");
+            Console.ReadKey();
         }
     }
 }
